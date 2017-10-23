d39e72a430eebf0efc040f01338ea68f80d206d2
rahulgayatri23 <rahulgayatri84@gmail.com>
2017-10-23 14:28:31 -0400

    Adding a tryOnMe.cpp and the corresponding Makefile

---
+++ tryOnMe.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    //OpenMP Printing of threads on Host and Device
+    int tid, numThreads, numTeams;
+#pragma omp parallel shared(numThreads) private(tid)
+    {
+        tid = omp_get_thread_num();
+        if(tid == 0)
+            numThreads = omp_get_num_threads();
+    }
+    std::cout << "Number of OpenMP Threads = " << numThreads << endl;
+
+#pragma omp target enter data map(alloc: numTeams, numThreads)
+#pragma omp target map(tofrom: numTeams, numThreads)
+#pragma omp teams shared(numTeams) private(tid)
+    {
+        tid = omp_get_team_num();
+        if(tid == 0)
+        {
+            numTeams = omp_get_num_teams();
+#pragma omp parallel 
+            {
+                int ttid = omp_get_thread_num();
+                if(ttid == 0)
+                    numThreads = omp_get_num_threads();
+            }
+        }
+    }
+#pragma omp target exit data map(delete: numTeams, numThreads)
+    return 0;
+}
