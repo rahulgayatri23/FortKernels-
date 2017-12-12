a133af6fa81d99172cfc81212500ec4f5ae38e1a
rahulgayatri23 <rahulgayatri84@gmail.com>
2017-12-12 18:41:49 -0500

    Adding Kokkos versions in BGW_Kernels dir

---
+++ BGW-Kernels/gppKerKokkosCuda.cpp
@@ -0,0 +1,362 @@
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+
+#include <iomanip>
+#include <cmath>
+#include <complex>
+#include <omp.h>
+#include <ctime>
+#include <chrono>
+#include "gppKerKokkos.h"
+
+#include "GPUComplex.h"
+
+using namespace std;
+
+
+KOKKOS_INLINE_FUNCTION
+void reduce_achstemp(int n1, int number_bands, ViewVectorTypeInt inv_igp_index, int ncouls, ViewVectorTypeComplex aqsmtemp, ViewVectorTypeComplex aqsntemp, ViewVectorTypeComplex I_eps_array, GPUComplex achstemp,  ViewVectorTypeInt indinv, int ngpown, ViewVectorTypeDouble vcoul)
+{
+    double to1 = 1e-6;
+    GPUComplex schstemp(0.0, 0.0);;
+
+    for(int my_igp = 0; my_igp< ngpown; my_igp++)
+    {
+        GPUComplex schs(0.0, 0.0);
+        GPUComplex matngmatmgp(0.0, 0.0);
+        GPUComplex matngpmatmg(0.0, 0.0);
+        GPUComplex mygpvar2(0.00, 0.00);
+        GPUComplex mygpvar1(0.00, 0.00);
+        int indigp = inv_igp_index(my_igp);
+        int igp = indinv(indigp);
+
+        if(!(igp > ncouls || igp < 0)){
+
+            mygpvar1 = GPUComplex_conj(aqsmtemp(n1*ncouls+igp));
+            mygpvar2 = aqsntemp(n1*ncouls+igp);
+
+
+
+            schs = I_eps_array(my_igp*ncouls+igp);
+            matngmatmgp = GPUComplex_product(mygpvar1, aqsntemp(n1*ncouls+igp));
+
+
+            if(GPUComplex_abs(schs) > to1)
+                GPUComplex_fma(schstemp, matngmatmgp, schs);
+            }
+            else 
+            {
+                for(int ig=1; ig<ncouls; ++ig)
+                {
+                    GPUComplex mult_result(GPUComplex_product(I_eps_array(my_igp*ncouls+ig) , mygpvar1));
+                    GPUComplex_fms(schstemp,aqsntemp(n1*ncouls+igp), mult_result); 
+                }
+            }
+
+        schstemp = GPUComplex_mult(schstemp, vcoul(igp), 0.5);
+        achstemp += schstemp;
+    }
+}
+
+KOKKOS_INLINE_FUNCTION
+void flagOCC_solver(double wxt, ViewVectorTypeComplex wtilde_array, int my_igp, int n1, ViewVectorTypeComplex aqsmtemp, ViewVectorTypeComplex aqsntemp, ViewVectorTypeComplex I_eps_array, GPUComplex &ssxt, GPUComplex &scht,int ncouls, int igp, int number_bands, int ngpown)
+{
+    GPUComplex expr0(0.00, 0.00);
+    GPUComplex expr(0.5, 0.5);
+    GPUComplex matngmatmgp(0.0, 0.0);
+    GPUComplex matngpmatmg(0.0, 0.0);
+
+    for(int ig=0; ig<ncouls; ++ig)
+    {
+        GPUComplex wtilde = wtilde_array(my_igp*ncouls+ig);
+        GPUComplex wtilde2 = GPUComplex_square(wtilde);
+        GPUComplex Omega2 = GPUComplex_product(wtilde2,I_eps_array(my_igp*ncouls+ig));
+        GPUComplex mygpvar1 = GPUComplex_conj(aqsmtemp(n1*ncouls+igp));
+        GPUComplex mygpvar2 = aqsmtemp(n1*ncouls+igp);
+        GPUComplex matngmatmgp = GPUComplex_product(aqsntemp(n1*ncouls+ig) , mygpvar1);
+        if(ig != igp) matngpmatmg = GPUComplex_product(GPUComplex_conj(aqsmtemp(n1*ncouls+ig)) , mygpvar2);
+
+        double ssxcutoff;
+        double to1 = 1e-6;
+        double sexcut = 4.0;
+        double limitone = 1.0/(to1*4.0);
+        double limittwo = pow(0.5,2);
+        GPUComplex sch(0.00, 0.00) ;
+        GPUComplex ssx(0.00, 0.00) ;
+    
+        GPUComplex wdiff = doubleMinusGPUComplex(wxt , wtilde);
+    
+        GPUComplex cden = wdiff;
+        double rden = 1/GPUComplex_real(GPUComplex_product(cden , GPUComplex_conj(cden)));
+        GPUComplex delw = GPUComplex_mult(GPUComplex_product(wtilde , GPUComplex_conj(cden)) , rden);
+        double delwr = GPUComplex_real(GPUComplex_product(delw , GPUComplex_conj(delw)));
+        double wdiffr = GPUComplex_real(GPUComplex_product(wdiff , GPUComplex_conj(wdiff)));
+    
+        if((wdiffr > limittwo) && (delwr < limitone))
+        {
+            sch = GPUComplex_product(delw , I_eps_array[my_igp*ngpown+ig]);
+            double cden = std::pow(wxt,2);
+            rden = std::pow(cden,2);
+            rden = 1.00 / rden;
+            ssx = GPUComplex_mult(Omega2 , cden , rden);
+        }
+        else if (delwr > to1)
+        {
+            sch = expr0;
+            cden = GPUComplex_mult(GPUComplex_product(wtilde2, doublePlusGPUComplex((double)0.50, delw)), 4.00);
+            rden = GPUComplex_real(GPUComplex_product(cden , GPUComplex_conj(cden)));
+            rden = 1.00/rden;
+            ssx = GPUComplex_product(GPUComplex_product(-Omega2 , GPUComplex_conj(cden)), GPUComplex_mult(delw, rden));
+        }
+        else
+        {
+            sch = expr0;
+            ssx = expr0;
+        }
+    
+        ssxcutoff = GPUComplex_abs(I_eps_array(my_igp*ngpown+ig)) * sexcut;
+        if((GPUComplex_abs(ssx) > ssxcutoff) && (wxt < 0.00)) ssx = expr0;
+
+        ssxt += GPUComplex_product(matngmatmgp , ssx);
+        scht += GPUComplex_product(matngmatmgp , sch);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    Kokkos::initialize(argc, argv);
+    {
+
+    if (argc != 5)
+    {
+        std::cout << "The correct form of input is : " << endl;
+        std::cout << " ./a.out <number_bands> <number_valence_bands> <number_plane_waves> <nodes_per_mpi_group> " << endl;
+        exit (0);
+    }
+    auto start_totalTime = std::chrono::high_resolution_clock::now();
+
+    int number_bands = atoi(argv[1]);
+    int nvband = atoi(argv[2]);
+    int ncouls = atoi(argv[3]);
+    int nodes_per_group = atoi(argv[4]);
+
+
+    int npes = 1; //Represents the number of ranks per node
+    int ngpown = ncouls / (nodes_per_group * npes); //Number of gvectors per mpi task
+
+    double e_lk = 10;
+    double dw = 1;
+    int nstart = 0, nend = 3;
+
+    double to1 = 1e-6, \
+    gamma = 0.5, \
+    sexcut = 4.0;
+    double limitone = 1.0/(to1*4.0), \
+    limittwo = pow(0.5,2);
+
+    double e_n1kq= 6.0; //This in the fortran code is derived through the double dimenrsion array ekq whose 2nd dimension is 1 and all the elements in the array have the same value
+
+    //Printing out the params passed.
+    std::cout << "number_bands = " << number_bands \
+        << "\t nvband = " << nvband \
+        << "\t ncouls = " << ncouls \
+        << "\t nodes_per_group  = " << nodes_per_group \
+        << "\t ngpown = " << ngpown \
+        << "\t nend = " << nend \
+        << "\t nstart = " << nstart \
+        << "\t gamma = " << gamma \
+        << "\t sexcut = " << sexcut \
+        << "\t limitone = " << limitone \
+        << "\t limittwo = " << limittwo << endl;
+
+
+    //ALLOCATE statements from fortran gppkernel.
+    
+   
+    GPUComplex expr0(0.00, 0.00);
+    GPUComplex expr(0.5, 0.5);
+
+    GPUComplex *acht_n1_loc = new GPUComplex[number_bands];
+    GPUComplex *achtemp = new GPUComplex[nend-nstart];
+    double *achtemp_re = new double[3];
+    double *achtemp_im = new double[3];
+    double occ=1.0;
+    GPUComplex achstemp;
+
+//Kokkos views Begin
+    //GPUComplex Views
+    ViewVectorTypeComplex wtilde_array("wtilde_array", ngpown*ncouls);
+    ViewVectorTypeComplex aqsmtemp("aqsmtemp", number_bands*ncouls);
+    ViewVectorTypeComplex aqsntemp("aqsntemp", number_bands*ncouls);
+    ViewVectorTypeComplex I_eps_array("I_eps_array", ngpown*ncouls);
+    ViewVectorTypeComplex asxtemp("asxtemp", nend-nstart);
+
+    //Doble views
+    ViewVectorTypeDouble wx_array("wx_array",3);
+    ViewVectorTypeDouble vcoul("vcoul",ncouls);
+//    ViewVectorTypeDouble achtemp_re_loc("achtemp_re_loc",3);
+//    ViewVectorTypeDouble achtemp_im_loc("achtemp_im_loc",3);
+
+    //Int Views
+    ViewVectorTypeInt inv_igp_index("inv_igp_index", ngpown);
+    ViewVectorTypeInt indinv("indinv", ncouls+1);
+
+
+//Host Views 
+    ViewVectorTypeComplex::HostMirror h_aqsmtemp = Kokkos::create_mirror_view(aqsmtemp);
+    ViewVectorTypeComplex::HostMirror h_aqsntemp = Kokkos::create_mirror_view(aqsntemp);
+    ViewVectorTypeComplex::HostMirror h_wtilde_array = Kokkos::create_mirror_view(wtilde_array);
+    ViewVectorTypeComplex::HostMirror h_I_eps_array = Kokkos::create_mirror_view(I_eps_array);
+    ViewVectorTypeComplex::HostMirror h_asxtemp = Kokkos::create_mirror_view(asxtemp);
+
+    ViewVectorTypeDouble::HostMirror h_wx_array = Kokkos::create_mirror_view(wx_array);
+    ViewVectorTypeDouble::HostMirror h_vcoul = Kokkos::create_mirror_view(vcoul);
+
+    ViewVectorTypeInt::HostMirror h_inv_igp_index = Kokkos::create_mirror_view(inv_igp_index);
+    ViewVectorTypeInt::HostMirror h_indinv = Kokkos::create_mirror_view(indinv);
+
+    cout << "Size of wtilde_array = " << (ncouls*ngpown*2.0*8) / pow(1024,2) << " Mbytes" << endl;
+    cout << "Size of aqsntemp = " << (ncouls*number_bands*2.0*8) / pow(1024,2) << " Mbytes" << endl;
+    cout << "Size of I_eps_array array = " << (ncouls*ngpown*2.0*8) / pow(1024,2) << " Mbytes" << endl;
+
+
+   for(int i=0; i<number_bands; i++)
+       for(int j=0; j<ncouls; j++)
+       {
+           h_aqsmtemp(i*ncouls+j) = expr;
+           h_aqsntemp(i*ncouls+j) = expr;
+       }
+
+   for(int i=0; i<ngpown; i++)
+       for(int j=0; j<ncouls; j++)
+       {
+           h_I_eps_array(i*ncouls+j) = expr;
+           h_wtilde_array(i*ncouls+j) = expr;
+       }
+
+   for(int i=0; i<ncouls; i++)
+       h_vcoul(i) = 1.0;
+
+//
+    for(int ig=0; ig < ngpown; ++ig)
+        h_inv_igp_index(ig) = (ig+1) * ncouls / ngpown;
+
+    //Do not know yet what this array represents
+    for(int ig=0, tmp=1; ig<ncouls; ++ig,tmp++)
+        h_indinv(ig) = ig;
+        h_indinv(ncouls) = ncouls-1;
+
+       for(int iw=nstart; iw<nend; ++iw)
+       {
+           h_asxtemp(iw) = expr0;
+           achtemp_re[iw] = 0.00;
+           achtemp_im[iw] = 0.00;
+       }
+
+        for(int iw=nstart; iw<nend; ++iw)
+        {
+            h_wx_array(iw) = e_lk - e_n1kq + dw*((iw+1)-2);
+            if(h_wx_array(iw) < to1) h_wx_array(iw) = to1;
+        }
+
+//Copy From host to device Views Kokkos::deep_copy( y, h_y )
+    Kokkos::deep_copy(aqsmtemp, h_aqsmtemp);
+    Kokkos::deep_copy(aqsntemp, h_aqsntemp);
+    Kokkos::deep_copy(wtilde_array, h_wtilde_array);
+    Kokkos::deep_copy(I_eps_array, h_I_eps_array);
+    Kokkos::deep_copy(asxtemp, h_asxtemp);
+    Kokkos::deep_copy(wx_array, h_wx_array);
+    Kokkos::deep_copy(vcoul, h_vcoul);
+    Kokkos::deep_copy(inv_igp_index, h_inv_igp_index);
+    Kokkos::deep_copy(indinv, h_indinv);
+
+   for(int n1 = 0; n1 < nvband; n1++)
+   {
+        Kokkos::parallel_for(ngpown,KOKKOS_LAMBDA (int my_igp)
+        {
+           for(int iw=nstart; iw<nend; iw++)
+           {
+                int indigp = inv_igp_index(my_igp);
+                int igp = indinv(indigp);
+                GPUComplex ssxt(0.00, 0.00);
+                GPUComplex scht(0.00, 0.00);
+                flagOCC_solver(wx_array(iw), wtilde_array, my_igp, n1, aqsmtemp, aqsntemp, I_eps_array, ssxt, scht, ncouls, igp, number_bands, ngpown);
+                asxtemp(iw) += GPUComplex_mult(ssxt, occ , vcoul(igp));
+            }
+        });
+   }
+
+    Kokkos::parallel_for(number_bands, KOKKOS_LAMBDA (int n1)
+    {
+        reduce_achstemp(n1, number_bands, inv_igp_index, ncouls,aqsmtemp, aqsntemp, I_eps_array, achstemp, indinv, ngpown, vcoul);
+    });
+
+    auto start_chrono = std::chrono::high_resolution_clock::now();
+
+    achtempStruct achtempVar;
+
+    Kokkos::parallel_reduce(team_policy(number_bands, Kokkos::AUTO, 32), KOKKOS_LAMBDA (const member_type &teamMember, achtempStruct& achtempVarUpdate)
+    {
+        const int n1 = teamMember.league_rank();
+
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(teamMember, ngpown), [&] (const int my_igp)
+        {
+            int indigp = inv_igp_index(my_igp);
+            int igp = indinv(indigp);
+            achtempStruct achtempVar_loc;
+
+            Kokkos::parallel_reduce( Kokkos::ThreadVectorRange(teamMember, ncouls), [&] (const int ig, achtempStruct& achtempVar_locUpdate)
+            {
+                for(int iw = 0; iw < 3; ++iw)
+                {
+                    GPUComplex wdiff = doubleMinusGPUComplex(wx_array(iw), wtilde_array(my_igp*ncouls+ig));
+                    GPUComplex delw = GPUComplex_mult(GPUComplex_product(wtilde_array(my_igp*ncouls+ig), GPUComplex_conj(wdiff)), 1/GPUComplex_real(GPUComplex_product(wdiff, GPUComplex_conj(wdiff)))); 
+                    GPUComplex sch_array = GPUComplex_mult(GPUComplex_product(GPUComplex_product(GPUComplex_conj(aqsmtemp(n1*ncouls+igp)), aqsntemp(n1*ncouls+ig)), GPUComplex_product(delw , I_eps_array(my_igp*ncouls+ig))), 0.5*vcoul(igp));
+                    achtempVar_locUpdate.achtemp_re[iw] += GPUComplex_real(sch_array);
+                    achtempVar_locUpdate.achtemp_im[iw] += GPUComplex_imag(sch_array);
+                }
+
+            }, achtempVar_loc);
+
+            for(int iw = nstart; iw < nend; ++iw)
+            {
+                achtempVarUpdate.achtemp_re[iw] += achtempVar_loc.achtemp_re[iw];
+                achtempVarUpdate.achtemp_im[iw] += achtempVar_loc.achtemp_im[iw];
+            }
+
+        }); //ngpown
+
+    }, achtempVar); // number-bands
+
+    for(int iw = nstart; iw < nend; ++iw)
+    {
+        achtemp_re[iw] = achtempVar.achtemp_re[iw];
+        achtemp_im[iw] = achtempVar.achtemp_im[iw];
+    }
+
+    std::chrono::duration<double> elapsed_chrono = std::chrono::high_resolution_clock::now() - start_chrono;
+
+    printf(" \n Final achstemp\n");
+    achstemp.print();
+
+    printf("\n Final achtemp\n");
+
+    for(int iw=nstart; iw<nend; ++iw)
+    {
+        GPUComplex tmp(achtemp_re[iw], achtemp_im[iw]);
+        achtemp[iw] = tmp;
+        achtemp[iw].print();
+    }
+
+    std::chrono::duration<double> elapsed_totalTime = std::chrono::high_resolution_clock::now() - start_totalTime;
+    cout << "********** Total Time Taken **********= " << elapsed_totalTime.count() << " secs" << endl;
+
+    free(acht_n1_loc);
+    free(achtemp);
+
+    }
+    Kokkos::finalize();
+
+    return 0;
+}
